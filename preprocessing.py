be4092d91a95900feec7354a9b7ed3306f202575
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2023-05-01 22:53:56 +0300

    preprocess

--- preprocessing.py
+++ preprocessing.py
@@ -6,6 +6,8 @@ import argparse
 from tqdm import tqdm
 import numpy as np
 import pandas as pd
+from datasets import load_dataset, Dataset
+from transformers import GPT2TokenizerFast
 
 
 def get_files(path):
@@ -51,12 +53,57 @@ def prepare_train_val(data):
 
     return train_data, val_data
 
+
+def chunk_text(text, max_chunk_size=1024, split_by="new_measure"):
+    sub_chunks = text.split(split_by)
+    for i in range(1, len(sub_chunks)):
+        sub_chunks[i] = split_by + sub_chunks[i]
+    sub_chunks = [s.strip() for s in sub_chunks]
+
+    chunks = [sub_chunks[0]]
+    chunk_i = 0
+    for i in range(1, len(sub_chunks)):
+        merged_chunk = chunks[chunk_i] + " " + sub_chunks[i]
+        if len(merged_chunk) <= max_chunk_size:
+            chunks[chunk_i] = merged_chunk
+        else:
+            chunks.append(sub_chunks[i])
+            chunk_i += 1
+
+    return chunks
+
+
+def chunk_map(examples):
+    chunked_texts = []
+    for text in examples["text"]:
+        chunked_texts += chunk_text(text)
+    examples["text"] = chunked_texts
+    return examples
+
+
+def prepare_dataset(path):
+    train_data = json.load(open(os.path.join(path, "train_data.json")))
+    val_data = json.load(open(os.path.join(path, "val_data.json")))
+    train_dataset = Dataset.from_list(train_data)
+    test_dataset = Dataset.from_list(val_data)
+    return train_dataset, test_dataset
+
+
+def tokenize_function(examples):
+    examples = tokenizer(examples["text"])
+    examples["labels"] = examples["input_ids"].copy()
+    return examples
+
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input-path', type=str, default='/mnt/e/Data/DadaGP-v1.1')
-    parser.add_argument('--output-path', type=str, default='/mnt/e/Data/DadaGP-processed')
-    parser.add_argument('--genre', type=str, default='classic_rock')
+    parser.add_argument("--input-path", type=str, default="/mnt/e/Data/DadaGP-v1.1")
+    parser.add_argument(
+        "--output-path", type=str, default="/mnt/e/Data/DadaGP-processed"
+    )
+    parser.add_argument("--genre", type=str, default="classic_rock")
     return parser.parse_args()
+
 
 def main():
     args = parse_arguments()
@@ -66,11 +113,35 @@ def main():
         df_metadata = filter(df_metadata, args.genre)
     train_data, val_data = prepare_train_val(df_metadata)
 
-    print(len(train_data), len(val_data))
-
     os.makedirs(args.output_path, exist_ok=True)
-    json.dump(train_data, open(os.path.join(args.output_path, 'train_data.json'), 'w'))
-    json.dump(val_data, open(os.path.join(args.output_path, 'val_data.json'), 'w'))
+    json.dump(train_data, open(os.path.join(args.output_path, "train_data.json"), "w"))
+    json.dump(val_data, open(os.path.join(args.output_path, "val_data.json"), "w"))
+
+    train_dataset, test_dataset = prepare_dataset(args.output_dataset)
+
+    train_dataset = train_dataset.map(
+        chunk_map,
+        batched=True,
+        num_proc=4,
+        remove_columns=["validation_set", "tokens.txt", "artist_token", "genre_tokens"],
+    )
+    train_dataset = train_dataset.map(
+        tokenize_function, batched=True, num_proc=4, remove_columns=["text"]
+    )
+
+    test_dataset = test_dataset.map(
+        chunk_map,
+        batched=True,
+        num_proc=4,
+        remove_columns=["validation_set", "tokens.txt", "artist_token", "genre_tokens"],
+    )
+    test_dataset = test_dataset.map(
+        tokenize_function, batched=True, num_proc=4, remove_columns=["text"]
+    )
+
+    train_dataset.save_to_disk(args.output_path)
+    test_dataset.save_to_disk(args.output_path)
+
 
 if __name__ == "__main__":
     main()
