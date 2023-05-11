ead7a18cd3c20e83db2d0790607c35595d43bc31
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2023-05-12 00:28:14 +0300

    conda init and partition

--- jobs/preprocessing.sh
+++ jobs/preprocessing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -p hamsi
+#SBATCH -p long
 #SBATCH --time=1-00:00:00
 #SBATCH -A tbag130
 #SBATCH -J tabgpt-preprocess
@@ -9,7 +9,7 @@
 #SBATCH -o /truba/home/tkarabulut/htgaa_project/out-%j.out
 #SBATCH -e /truba/home/tkarabulut/htgaa_project/err-%j.err
 
-conda init
+conda init bash
 conda activate htgaa
 python preprocessing.py --input-path ../dadaGP-v1.1 \
                         --output-path ../DadaGP-processed-classic_rock \
