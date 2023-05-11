d2686218a7be1e92d25499678c3f7013199ef58b
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2023-05-11 22:57:22 +0300

    added slurm script1

---
+++ jobs/preprocessing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -p akya-cuda
+#SBATCH --time=1-00:00:00
+#SBATCH -A tbag130
+#SBATCH -J tabgpt-preprocess
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --cpus-per-task 10
+#SBATCH -o /truba/home/tkarabulut/htgaa_project/out-%j.out
+#SBATCH -e /truba/home/tkarabulut/htgaa_project/err-%j.err
+
+
+conda activate htgaa
+python preprocessing.py --input-path ../dadaGP-v1.1 \
+                        --output-path ../DadaGP-processed-classic_rock \
+                        --genre classic_rock
