b78d6648d179d1d15105252652d2ff8cc2884a86
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2023-05-12 20:26:27 +0300

    conda bugfix

--- jobs/preprocessing.sh
+++ jobs/preprocessing.sh
@@ -9,7 +9,7 @@
 #SBATCH -o /truba/home/tkarabulut/htgaa_project/out-%j.out
 #SBATCH -e /truba/home/tkarabulut/htgaa_project/err-%j.err
 
-conda init bash
+eval "$(conda shell.bash hook)"
 conda activate htgaa
 python preprocessing.py --input-path ../dadaGP-v1.1 \
                         --output-path ../DadaGP-processed-classic_rock \
