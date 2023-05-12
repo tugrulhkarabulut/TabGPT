cd36188208574d1aa32d19ce7e24c2cdab7519c2
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2023-05-12 21:53:10 +0300

    changed logs dir, fix dir name

--- jobs/preprocessing.sh
+++ jobs/preprocessing.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #SBATCH -p long
 #SBATCH --time=1-00:00:00
 #SBATCH -A tbag130
@@ -6,10 +6,10 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --cpus-per-task 4
-#SBATCH -o /truba/home/tkarabulut/htgaa_project/out-%j.out
-#SBATCH -e /truba/home/tkarabulut/htgaa_project/err-%j.err
+#SBATCH -o /truba/home/tkarabulut/htgaa_project/TabGPT/logs/out-%j.out
+#SBATCH -e /truba/home/tkarabulut/htgaa_project/TabGPT/logs/err-%j.err
 
-eval "$(conda shell.bash hook)"
+#eval "$(conda shell.bash hook)"
 conda activate htgaa
 python preprocessing.py --input-path ../DadaGP-v1.1 \
                         --output-path ../DadaGP-processed-classic_rock \
