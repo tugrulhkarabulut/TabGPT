84e6478ae75dcf4aa25ed3696e6de4b08ebfdaaf
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2023-05-13 14:09:40 +0300

    train job revert

--- jobs/train.sh
+++ jobs/train.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-#SBATCH -p palamut-cuda
+#SBATCH -p akya-cuda
 #SBATCH --time=1-00:00:00
 #SBATCH -A tbag130
 #SBATCH -J tabgpt-train
 #SBATCH --gres=gpu:1
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --cpus-per-task 16
+#SBATCH --cpus-per-task 10
 #SBATCH -o /truba/home/tkarabulut/htgaa_project/TabGPT/logs/out-train-%j.out
 #SBATCH -e /truba/home/tkarabulut/htgaa_project/TabGPT/logs/out-train-%j.err
 
