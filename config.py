8cc0169545e6756debf21021ed8e39933b5a2f3d
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2023-05-13 14:32:08 +0300

    updated config definition

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ _C.OUTPUT = "./output/"
 _C.MODEL = "gpt2"
 _C.RESUME_FROM_CKPT = False
 _C.CKPT_PATH = ""
+_C.USE_PEFT = False
 
 _C.DATA = CN()
 _C.DATA.TRAIN_DATASET = "train_dataset"
@@ -23,6 +24,11 @@ _C.SOLVER.GRAD_CKPT = True
 _C.SOLVER.FP16 = True
 _C.SOLVER.EPOCHS = 10
 _C.SOLVER.WEIGHT_DECAY = 1e-2
+
+_C.PEFT = CN()
+_C.PEFT.LORA_ALPHA = 32
+_C.PEFT.LORA_DROPOUT = 0.1
+_C.PEFT.DIM_R = 8
 
 
 def get_cfg_defaults():
